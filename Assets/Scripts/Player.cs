eda4761b5fa608eddf89c9b9bc953f4b9fa9a96c
RobertsSiklis <75177703+RobertsSiklis@users.noreply.github.com>
2023-08-16 07:40:54 +0300

    Ground check implemented(Not Working)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,16 +5,20 @@ using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private LayerMask groundLayerMask;
     [SerializeField] private float movementSpeed = 10f;
+    private bool isGrounded;
+    private bool isJumping;
     private Vector2 inputVector;
     private Rigidbody2D rb;
+    
 
     private void Awake() {
         rb = GetComponent<Rigidbody2D>();
     }
 
     private void Update() {
-
+        CastGroundCheckRay();
         inputVector = new Vector2(0, 0);
         if (Input.GetKey(KeyCode.A)) {
             inputVector = new Vector2(-1, 0);
@@ -22,14 +26,39 @@ public class Player : MonoBehaviour
         if (Input.GetKey(KeyCode.D)) {
             inputVector = new Vector2(1, 0);
         }
-
+        if (Input.GetKey(KeyCode.Space)) {
+            isJumping = true;
+        }
+        isJumping = false;
+        
     }
+
+    private void CastGroundCheckRay() {
+        RaycastHit2D ray = Physics2D.Raycast(transform.position + new Vector3(0, -0.6f, 0), Vector2.down, 1f, groundLayerMask);
+        if (ray.collider != null) {
+            isGrounded = true;
+        }
+        isGrounded = false;
+    }
+
 
     private void FixedUpdate() {
-        rb.MovePosition(rb.position + movementSpeed * Time.fixedDeltaTime * inputVector);
+        MovePlayer();
+        Debug.Log(isGrounded + " " + isJumping);
+        if (isGrounded && isJumping) {
+            Jump();
+        }
     }
 
+    private void MovePlayer() {
+        rb.MovePosition(rb.position + movementSpeed * Time.fixedDeltaTime * inputVector);
+    }
+    private void Jump() {
+        rb.AddForce(transform.up * 2f, ForceMode2D.Impulse);
+    }
+
+
     private void OnDrawGizmos() {
-        Gizmos.DrawRay(transform.position, new Vector3(0, -3f));
+        Gizmos.DrawRay(transform.position + new Vector3(0, -0.6f, 0), new Vector3(0, -2f, 0));
     }
 }
